d949ba1867340942b3cd10ace71ea475ba291986
Songwaad <Songw44d@gmail.com>
2025-12-04 08:40:22 +0700

    feat: refactor App component and set up routing with Login and SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import './App.css'
-import { Button } from './components/ui/button'
-
 function App() {
+
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+      <div>App.tsx</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import './index.css'
-import App from './App.tsx'
+
+import LoginPage from './pages/LoginPage.tsx';
+import SignUpPage from './pages/SignUpPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
+  </BrowserRouter>,
 )
