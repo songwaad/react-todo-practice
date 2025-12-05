16fc50460226e4f3ca1fa44895fdedf330896606
Songwaad <Songw44d@gmail.com>
2025-12-05 07:48:18 +0700

    feat: update routing to include App component and enhance navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router";
+
 function App() {
 
   return (
     <>
-      <div>App.tsx</div>
+      <div>App.tsx</div><br />
+      <Link to="/login">Go to Login Page</Link>
+      <br />
+      <Link to="/signup">Go to Sign Up Page</Link>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 
 import './index.css'
 
+import { AuthProvider } from './context/AuthContext.tsx';
 import LoginPage from './pages/LoginPage.tsx';
 import SignUpPage from './pages/SignUpPage.tsx';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </AuthProvider>
   </BrowserRouter>,
 )
