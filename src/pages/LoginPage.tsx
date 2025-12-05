5211b3b40566926150b4a64d578f9714f1a01180
Songwaad <Songw44d@gmail.com>
2025-12-05 07:48:12 +0700

    feat: add SignUpPage component with form validation and user registration

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+
+import { useNavigate } from "react-router";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { useAuth } from "@/hooks/useAuth";
+
+import { Form, FormControl, FormField, FormLabel, FormMessage } from "@/components/ui/form";
+
+import "../App.css";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+
+//สร้าง Schema สำหรับ validate account
+const loginFormSchema = yup.object().shape({
+  username: yup.string().required("This field is required."),
+  password: yup.string().required("This field is required."),
+});
+
+function LoginPage() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loginForm = useForm<{ username: string; password: string }>({
+    resolver: yupResolver(loginFormSchema),
+    mode: "onBlur",
+    defaultValues: {
+      username: "json",
+      password: "password",
+    },
+  });
+
+  const onSubmit: SubmitHandler<{ username: string; password: string }> = async (data) => {
+    setIsLoading(true);
+
+    const result = await login(data.username, data.password);
+    if (result.success) {
+      navigate("/");
+    } else {
+      alert(`Login failed: ${result.error}`);
+    }
+    setIsLoading(false);
+  }
+
+  const img =
+    "https://images.unsplash.com/photo-1552083974-186346191183?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="flex-[6]">
+          <img className="h-full" src={img} alt="image" />
+        </div>
+
+        <div className="flex-[4]">
+          <div className="flex h-full items-center">
+
+            {/* FORM */}
+            <Form {...loginForm}>
+              <form
+                onSubmit={loginForm.handleSubmit(onSubmit)}
+                className="flex flex-col m-auto w-full items-center "
+              >
+                <FormField
+                  control={loginForm.control}
+                  name="username"
+                  render={({ field }) => (
+                    <div className="grid w-full max-w-sm items-center gap-3 pb-4">
+                      <FormLabel> Username </FormLabel>
+                      <FormControl>
+                        <Input
+                          id="username"
+                          type="text" 
+                          placeholder="Username"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </div>
+                  )}
+                />
+
+                <FormField 
+                  control={loginForm.control}
+                  name="password"
+                  render={({ field }) => (
+                    <div className="grid w-full max-w-sm items-center gap-3 pb-4">
+                      <FormLabel> Password </FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input 
+                            id="password"
+                            type={showPassword ? "text" : "password"} 
+                            placeholder="Password"
+                            {...field}
+                          />
+                          <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer hover:opacity-80"
+                          >
+                          {showPassword ? (
+                            <EyeOffIcon size={18} strokeWidth={0.7} />
+                          ) : (
+                            <EyeIcon size={18} strokeWidth={0.7} />
+                          )}
+                          </button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </div>
+                  )}
+                />
+
+                <div className="grid w-full max-w-sm items-center gap-3 pt-4">
+                  <Button type="submit" className="cursor-pointer w-full">
+                    {isLoading ? (
+                      <>
+                        <Spinner /> Loading...
+                      </>
+                    ) : (
+                      "Login"
+                    )}
+                  </Button>
+                </div>
+
+                <div className="grid w-full max-w-sm items-center text-center gap-3 pt-4">
+                  <p className="text-sm">
+                    Don't Have an account?{" "}
+                    <a href="/signup" className="text-blue-500 hover:underline">
+                      Sign up
+                    </a>
+                  </p>
+                </div>
+              </form>
+            </Form>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,169 @@
+import { useState } from "react";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+
+import { useForm, type SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { useNavigate } from "react-router";
+import { apiRegister } from "@/api/auth";
+
+import { Form, FormControl, FormField, FormLabel, FormMessage } from "@/components/ui/form";
+
+import "../App.css";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+
+//สร้าง Schema สำหรับ validate account
+const signUpFormSchema = yup.object().shape({
+  username: yup.string().required("This field is required."),
+  password: yup.string()
+    .required("This field is required.")
+    .min(6, "Password must be at least 6 characters long."),
+  confirmPassword: yup.string()
+    .oneOf([yup.ref('password')], 'Passwords must match')
+    .required("This field is required.")
+    .min(6, "Password must be at least 6 characters long."),
+});
+
+function SignUpPage() {
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const registerForm = useForm<{ username: string; password: string; confirmPassword: string }>({
+    resolver: yupResolver(signUpFormSchema),
+    mode: "onBlur",
+    defaultValues: {
+      username: "username",
+      password: "password",
+      confirmPassword: "password",
+    }
+  });
+  const onSubmit: SubmitHandler<{ username: string; password: string }> = async (
+    data
+  ) => {
+    setIsLoading(true);
+
+    const res = await apiRegister(data.username, data.password);
+    if (res.ok) {
+      navigate("/login");
+    } else {
+      alert(`Registration failed: ${res.error}`);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+
+        <div className="flex items-center justify-center h-screen">
+          <div className="flex w-lg py-14 border rounded-3xl items-center">
+
+            {/* FORM */}
+            <Form {...registerForm}>
+              <form
+                onSubmit={registerForm.handleSubmit(onSubmit)}
+                className="flex flex-col m-auto w-full items-center "
+              >
+                <FormField
+                  control={registerForm.control}
+                  name="username"
+                  render={({ field }) => (
+                    <div className="grid w-full max-w-sm items-center gap-3 pb-4">
+                      <FormLabel> Username </FormLabel>
+                      <FormControl>
+                        <Input type="text" placeholder="Username" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </div>
+                  )}
+                />
+
+                <FormField 
+                  control={registerForm.control}
+                  name="password"
+                  render={({ field }) => (
+                    <div className="grid w-full max-w-sm items-center gap-3 pb-4">
+                      <FormLabel> Password </FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input type={showPassword ? "text" : "password"} placeholder="Password" {...field} />
+                          <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer hover:opacity-80"
+                          >
+                          {showPassword ? (
+                            <EyeOffIcon size={18} strokeWidth={0.7} />
+                          ) : (
+                            <EyeIcon size={18} strokeWidth={0.7} />
+                          )}
+                          </button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </div>
+                  )}
+                />
+
+                <FormField 
+                  control={registerForm.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <div className="grid w-full max-w-sm items-center gap-3 pb-4">
+                      <FormLabel> Confirm Password </FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input type={showPassword ? "text" : "password"} placeholder="Confirm Password" {...field} />
+                          <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer hover:opacity-80"
+                          >
+                          {showPassword ? (
+                            <EyeOffIcon size={18} strokeWidth={0.7} />
+                          ) : (
+                            <EyeIcon size={18} strokeWidth={0.7} />
+                          )}
+                          </button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </div>
+                  )}
+                />
+
+                <div className="grid w-full max-w-sm items-center gap-3 pt-4">
+                  <Button type="submit" className="cursor-pointer w-full">
+                    {isLoading ? (
+                      <>
+                        <Spinner /> Loading...
+                      </>
+                    ) : (
+                      "Sign Up"
+                    )}
+                  </Button>
+                </div>
+
+                <div className="grid w-full max-w-sm items-center text-center gap-3 pt-4">
+                  <p className="text-sm">
+                    Have an account?{" "}
+                    <a href="/login" className="text-blue-500 hover:underline">
+                      Login
+                    </a>
+                  </p>
+                </div>
+              </form>
+            </Form>
+
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default SignUpPage;
